9b432cc1cce9135736d5bf01a0fd6e19bd7cab07
viacheslev <viacheslevdenmark@gmail.com>
2026-09-16 14:50:50 +0200

    add reviews

--- app/js/main.js
+++ app/js/main.js
@@ -5,8 +5,27 @@ const swiper = new Swiper('.accessories__slider', {
   spaceBetween: 40,
   // Navigation arrows
   navigation: {
-    nextEl: '.accessories__arrow-prev',
-    prevEl: '.accessories__arrow-next',
+    nextEl: '.accessories__arrow-next',
+    prevEl: '.accessories__arrow-prev',
+  },
+
+});
+
+const swiperReview = new Swiper('.reviews__slider', {
+
+  loop: true,
+  slidesPerView: 9,
+  spaceBetween: 16,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews__arrow-next',
+    prevEl: '.reviews__arrow-prev',
+  },
+
+  pagination: {
+    el: '.reviews__pagination',
+    type: 'fraction',
   },
 
 });
