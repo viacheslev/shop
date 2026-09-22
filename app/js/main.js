841df8d3de93883a04c4cfb6c385789879786109
viacheslev <viacheslevdenmark@gmail.com>
2026-09-22 18:15:45 +0200

    add code optimization

--- app/js/main.js
+++ app/js/main.js
@@ -1,86 +1,97 @@
 const headerBtn = document.querySelector('.header__btn');
 const menu = document.querySelector('.menu');
 
-headerBtn.addEventListener('click', () => {
-  menu.classList.toggle('menu--active');
-});
+if (headerBtn && menu) {
+  headerBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu--active');
+  });
+}
 
 const modeContainer = document.querySelector('.view-mode__container');
 const modeBtnGrid = document.querySelector('.view-mode__btn-grid');
 const modeBtnLine = document.querySelector('.view-mode__btn-line');
 
-modeBtnGrid.addEventListener('click', () => {
-  modeContainer.classList.add('view-mode__container--grid')
-  modeContainer.classList.remove('view-mode__container--line')
-})
-modeBtnLine.addEventListener('click', () => {
-  modeContainer.classList.add('view-mode__container--line')
-  modeContainer.classList.remove('view-mode__container--grid')
-})
+if (modeContainer && modeBtnGrid && modeBtnLine) {
+  modeBtnGrid.addEventListener('click', () => {
+    modeContainer.classList.add('view-mode__container--grid');
+    modeContainer.classList.remove('view-mode__container--line');
+  });
 
-const swiper = new Swiper('.accessories__slider', {
+  modeBtnLine.addEventListener('click', () => {
+    modeContainer.classList.add('view-mode__container--line');
+    modeContainer.classList.remove('view-mode__container--grid');
+  });
+}
 
-  loop: true,
-  slidesPerView: 3,
-  spaceBetween: 40,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.accessories__arrow-next',
-    prevEl: '.accessories__arrow-prev',
-  },
 
-});
+const accessoriesSlider = document.querySelector('.accessories__slider');
 
-const swiperReview = new Swiper('.reviews__slider', {
+if (accessoriesSlider) {
+  new Swiper(accessoriesSlider, {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 40,
 
-  loop: true,
-  slidesPerView: 9,
-  spaceBetween: 16,
+    navigation: {
+      nextEl: '.accessories__arrow-next',
+      prevEl: '.accessories__arrow-prev',
+    },
+  });
+}
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews__arrow-next',
-    prevEl: '.reviews__arrow-prev',
-  },
 
-  pagination: {
-    el: '.reviews__pagination',
-    type: 'fraction',
-  },
+const reviewsSlider = document.querySelector('.reviews__slider');
 
-});
+if (reviewsSlider) {
+  new Swiper(reviewsSlider, {
+    loop: true,
+    slidesPerView: 9,
+    spaceBetween: 16,
+
+    navigation: {
+      nextEl: '.reviews__arrow-next',
+      prevEl: '.reviews__arrow-prev',
+    },
+
+    pagination: {
+      el: '.reviews__pagination',
+      type: 'fraction',
+    },
+  });
+}
+
 
 const rangeSlider = document.querySelector('.range__slider');
 const rangeMin = document.querySelector('.range__min');
 const rangeMax = document.querySelector('.range__max');
 
-noUiSlider.create(rangeSlider, {
-  start: [300, 3000],
-  step: 100,
-  range: {
-    'min': 300,
-    'max': 3000
-  },
-  format: {
-    to: value => Math.round(value),
-    from: value => Number(value)
-  }
-});
+if (rangeSlider && rangeMin && rangeMax) {
+  noUiSlider.create(rangeSlider, {
+    start: [300, 3000],
+    step: 100,
+    range: {
+      min: 300,
+      max: 3000
+    },
+    format: {
+      to: value => Math.round(value),
+      from: value => Number(value)
+    }
+  });
 
-rangeSlider.noUiSlider.on('update', (values, handle) => {
-  if (handle === 0) {
-    rangeMin.value = values[0];
-  } else {
-    rangeMax.value = values[1];
-  }
-});
+  rangeSlider.noUiSlider.on('update', (values, handle) => {
+    if (handle === 0) {
+      rangeMin.value = values[0];
+    } else {
+      rangeMax.value = values[1];
+    }
+  });
 
-rangeMin.addEventListener('change', () => {
-  rangeSlider.noUiSlider.set([rangeMin.value, null])
-});
+  rangeMin.addEventListener('change', () => {
+    rangeSlider.noUiSlider.set([rangeMin.value, null]);
+  });
 
-rangeMax.addEventListener('change', () => {
-  rangeSlider.noUiSlider.set([null, rangeMax.value])
-});
-
-
+  rangeMax.addEventListener('change', () => {
+    rangeSlider.noUiSlider.set([null, rangeMax.value]);
+  });
+}
